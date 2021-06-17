ffe408cee42862bf98a6ee18377a6b93a5c78ba0
Owen Medina <owensmedi@gmail.com>
2021-06-17 17:57:59 +0800

    Create Main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./sass/components/App.module.scss";
 import Navbar from "./components/Navbar";
+import Main from "./components/Layout/Main";
 
 function App() {
   return (
     <div className={styles["App"]}>
       <Navbar />
+      <Main />
     </div>
   );
 }
